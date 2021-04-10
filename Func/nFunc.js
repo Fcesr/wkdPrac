4b70b89e2ec3f155ddae4c2b4c037cc72b534cc8
Fcesr <fmcesar@gmail.com>
2021-04-10 14:51:37 -0400

    my example of named function

---
+++ Func/nFunc.js
@@ -0,0 +1,12 @@
+// write function that dived two numbers 
+// created two varieables
+let num1 = 8
+let num2 = 6
+// created a functions that add values
+function addValue(x,y) {
+    let val1 = x/y
+    // displayes output
+    console.log( "this is a name function dived value " , val1)
+}
+// called function
+addValue(8,6)
